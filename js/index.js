17f6a23d78ebc67a3a4b4913b52687cad24bf294
sakibHasan7439 <sakibhasanb120@gmail.com>
2024-09-23 15:46:47 +0600

    all section modification completed

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const accountBalance = getInnerHTMLToNumber("balance");
+
 const donationSectionBtn = getElementById("donation");
 const historyBtn = getElementById("history");
 
@@ -11,91 +11,22 @@ const modal = document.getElementById("my_modal_1");
 const noakhaliDonateBtn = getElementById("noakhali-donate-btn");
 
 noakhaliDonateBtn.addEventListener("click", ()=>{
-    let noakhaliDonationAmount = getInnerHTMLToNumber("noakhali-donation-amount");
-    let noakhaliDonationInput = getInputElementById("noakhali-donation-input");
-
-        if(isNaN(noakhaliDonationInput) || noakhaliDonationInput < 1 || noakhaliDonationInput === ""){
-            alert("Invalid input");
-            return;
-    
-        }else if(noakhaliDonationInput > accountBalance){
-            alert("dontation amount cannot be greater then main balance");
-            return;
-        }
-
-
-    document.getElementById("balance").innerText -=  noakhaliDonationInput;
-
-    let donateUpdate = noakhaliDonationInput + noakhaliDonationAmount;
-    document.getElementById("noakhali-donation-amount").innerText = donateUpdate;
-    modal.classList.add("modal-open");
-
-    historySection.innerHTML += `
-        <div class="border-2 rounded-md mb-4 p-4">
-            <h3 class="font-bold mb-3">${noakhaliDonationInput} taka donate for flood at Noakhali, Bangladesh</h3>
-            <p>Date: ${Date()}</p>
-        </div>
-    `
+    sectionCalculator("noakhali-donation-amount", "noakhali-donation-input");
 })
 
 // feni seciton started
 const feniDonateBtn = getElementById("feni-donate-btn");
 
 feniDonateBtn.addEventListener("click", ()=>{
-    const feniDonationAmout = getInnerHTMLToNumber("feni-donation-amount");
-    const feniDonationInput = getInputElementById("feni-donation-input");
-    
-    if(isNaN(feniDonationInput) || feniDonationInput < 1 || feniDonationInput === ""){
-        alert("Invalid input");
-        return;
-
-    }else if(feniDonationInput > accountBalance){
-        alert("dontation amount cannot be greater then main balance");
-        return;
-    }
-
-    document.getElementById("balance").innerText -=  feniDonationInput;
-
-    let donateUpdate = feniDonationInput + feniDonationAmout;
-    document.getElementById("feni-donation-amount").innerText = donateUpdate;
-    modal.classList.add("modal-open");
-
-    historySection.innerHTML += `
-        <div class="border-2 rounded-md mb-4 p-4">
-            <h3 class="font-bold mb-3">${feniDonationInput} taka donate for flood at Feni, Bangladesh</h3>
-            <p>Date: ${Date()}</p>
-        </div>
-    `
+    sectionCalculator("feni-donation-amount", "feni-donation-input")
 })
 
 // Quota donation section started
 const quotaDonateBtn = getElementById("quota-donate-btn");
 
 quotaDonateBtn.addEventListener("click", ()=>{
-    const quotaDonationAmount = getInnerHTMLToNumber("quota-donation-amount");
-    const quotaDonationInput = getInputElementById("quota-donation-input");
+    sectionCalculator("quota-donation-amount", "quota-donation-input")
 
-    if(isNaN(quotaDonationInput) || quotaDonationInput < 1 || quotaDonationInput === ""){
-        alert("Invalid input");
-        return;
-
-    }else if(quotaDonationInput > accountBalance){
-        alert("dontation amount cannot be greater then main balance");
-        return;
-    }
-
-    document.getElementById("balance").innerText -=  quotaDonationInput;
-
-    let donateUpdate = quotaDonationInput + quotaDonationAmount;
-    document.getElementById("quota-donation-amount").innerText = donateUpdate;
-    modal.classList.add("modal-open");
-
-    historySection.innerHTML += `
-        <div class="border-2 rounded-md mb-4 p-4">
-            <h3 class="font-bold mb-3">${quotaDonationInput} taka donate for the Quota injured, Bangladesh</h3>
-            <p>Date: ${Date()}</p>
-        </div>
-    `
 })
 
 // modal close button 
--- js/utility.js
+++ js/utility.js
@@ -20,5 +20,34 @@ function showSection(id){
 }
 
 
+const accountBalance = getInnerHTMLToNumber("balance");
+
+function sectionCalculator(id1, id2){
+    const currentAmount = getInnerHTMLToNumber(id1);
+    const inputAmount = getInputElementById(id2);
+
+    if(isNaN(inputAmount) || inputAmount < 1 || inputAmount === ""){
+        alert("Invalid input");
+        return;
+
+    } if(inputAmount > document.getElementById("balance").innerText){
+        alert("insufficient balance");
+        return;
+    }
+
+    document.getElementById("balance").innerText -= inputAmount;
+
+    let donateUpdate = inputAmount + currentAmount;
+    document.getElementById(id1).innerText = donateUpdate;
+    modal.classList.add("modal-open");
+
+    historySection.innerHTML += `
+        <div class="border-2 rounded-md mb-4 p-4">
+            <h3 class="font-bold mb-3">${inputAmount} taka donate for flood at Noakhali, Bangladesh</h3>
+            <p>Date: ${Date()}</p>
+        </div>
+    `
+}
+
 
 
