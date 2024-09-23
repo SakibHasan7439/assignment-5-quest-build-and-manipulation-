0302707210c0304270d77e21189aeb12c0794172
sakibHasan7439 <sakibhasanb120@gmail.com>
2024-09-23 09:21:16 +0600

    feni section completed

--- js/index.js
+++ js/index.js
@@ -5,9 +5,6 @@ const historyBtn = getElementById("history");
 const donationSection = getElementById("donation-section");
 const historySection = getElementById("history-section");
 
-// const feniDonationAmout = getInnerHTMLToNumber("feni-donation-amount");
-// const feniDonateBtn = document.getElementById("feni-donate-btn");
-// const feniDonattionInput = getInputElementById("feni-donation-input");
 
 // const quotaDonationAmount = getInnerHTMLToNumber("quota-donation-amount");
 // const quotaDonationInput = getInputElementById("quota-donation-input");
@@ -30,6 +27,36 @@ noakhaliDonateBtn.addEventListener("click", ()=>{
     historySection.innerHTML += `
         <div class="border-2 rounded-md mb-4 p-4">
             <h3 class="font-bold mb-3">${noakhaliDonationInput} taka donate for flood at Noakhali, Bangladesh</h3>
+            <p>Date: ${Date()}</p>
+        </div>
+    `
+})
+
+historyBtn.addEventListener("click", (event)=>{
+
+    event.target.classList.add("rounded-md", "hover:bg-[#97d547]", "transition-colors", "duration-300", "bg-[#B4F461]", "font-bold", "rounded-md");
+
+    donationSectionBtn.classList.remove("rounded-md", "hover:bg-[#97d547]", "transition-colors", "duration-300", "bg-[#B4F461]", "font-bold");
+
+    showSection("history-section");
+});
+
+// feni seciton started
+
+const feniDonateBtn = document.getElementById("feni-donate-btn");
+
+feniDonateBtn.addEventListener("click", ()=>{
+    const feniDonationAmout = getInnerHTMLToNumber("feni-donation-amount");
+    const feniDonationInput = getInputElementById("feni-donation-input");
+
+    document.getElementById("balance").innerText -=  feniDonationInput;
+
+    let donateUpdate = feniDonationInput + feniDonationAmout;
+    document.getElementById("feni-donation-amount").innerText = donateUpdate;
+
+    historySection.innerHTML += `
+        <div class="border-2 rounded-md mb-4 p-4">
+            <h3 class="font-bold mb-3">${feniDonationInput} taka donate for flood at Feni, Bangladesh</h3>
             <p>Date: ${Date()}</p>
         </div>
     `
